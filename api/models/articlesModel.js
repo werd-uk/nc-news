5509010144db7cc077844fed97cb052febeb4e50
Drew <drew@werd.uk>
2025-01-28 12:12:53 +0000

    correct the camel case syntax in the model file.

--- api/models/articlesModel.js
+++ api/models/articlesModel.js
@@ -1,13 +1,13 @@
 const db = require("../../db/connection");
 
 exports.selectArticleByID = (articleID) => {
-    let SQLQuery = "SELECT author, title, body, article_id, topic, created_at, votes, article_img_url FROM articles";
+    let sqlQuery = "SELECT author, title, body, article_id, topic, created_at, votes, article_img_url FROM articles";
     const args = [];
     if (articleID) {
-        SQLQuery += " WHERE article_id = $1";
+        sqlQuery += " WHERE article_id = $1";
         args.push(articleID);
     }
-    return db.query(SQLQuery, args).then((response) => {
+    return db.query(sqlQuery, args).then((response) => {
         if (response.rows.length === 0) {
             return Promise.reject({ status: 404, msg: "No article found", detail: "Article #" + articleID + " does not exist" });
         } else {
