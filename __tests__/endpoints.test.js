c0ca3ce612ed41f0e976ec9153839c64cc596415
Drew <drew@werd.uk>
2025-01-29 17:53:35 +0000

    remove the console log

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -238,7 +238,6 @@ describe("POST /api/articles/:article_id/comments", () => {
             .expect(201)
             .then((response) => {
                 const comment = response.body.posted[0];
-                console.log(comment);
                 expect(comment.body).toEqual(testObject.body);
             });
     });
