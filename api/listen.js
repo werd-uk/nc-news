6051510e5c45920c1b77ab894c503b2be92c2f8a
Drew <drew@werd.uk>
2025-01-30 17:23:35 +0000

    prepare repo for production

--- api/listen.js
+++ api/listen.js
@@ -1,6 +1,4 @@
-const app = require("./app");
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(3000, (err) => {
-    if (err) console.log("Error in starting listener");
-    console.log("listening on port 3000");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,17 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
+require("dotenv").config({
+    path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+if (ENV === "production") {
+    config.max = 2;
+    config.connectionString = process.ENV.DATABASE_URL;
+}
+
+module.exports = new Pool(config);
