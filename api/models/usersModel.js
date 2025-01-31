896baaf1d53519081a5524fedaa044e30a69eb80
Drew <drew@werd.uk>
2025-01-31 15:36:51 +0000

    complete the test for existing and non-existing user search

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -2,7 +2,11 @@ const db = require("../../db/connection");
 
 exports.selectUserName = (username) => {
     return db.query("SELECT * FROM users WHERE username = $1", [username]).then((response) => {
-        return { user: response.rows[0] };
+        if (response.rowCount === 0) {
+            return Promise.reject({ status: 404, msg: "Not found", detail: `${username} does not exist.` });
+        } else {
+            return { user: response.rows[0] };
+        }
     });
 };
 
