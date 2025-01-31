428ae068da35a6267d790cdce31251782c9f6991
Drew <drew@werd.uk>
2025-01-31 10:14:38 +0000

    remove todo comments

--- api/errorHandling.js
+++ api/errorHandling.js
@@ -1,5 +1,4 @@
 exports.errorHandling = (err, req, res, next) => {
-    // TODO: Build error handling, once you've built functions / endpoints
     if ([404, 400, 403].includes(err.status)) {
         return res.status(err.status).send({ msg: err.msg, detail: err.detail });
     } else {
@@ -8,7 +7,6 @@ exports.errorHandling = (err, req, res, next) => {
 };
 
 exports.errorCatchAll = (err, req, res) => {
-    // TODO: Build error handling, once you've built functions / endpoints
     if (err) {
         return res.status(500).send({ msg: "Server Error", detail: err });
     }
